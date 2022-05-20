cb9463a303d43ab232ebda236bfe58bdf9552269
prb01 <p.bergstroem@gmail.com>
2022-05-20 11:37:14 -0500

    11.9 Simple end to end -tests

--- cypress/integration/1-getting-started/pokedex.test.js
+++ cypress/integration/1-getting-started/pokedex.test.js
@@ -1,16 +1,16 @@
-describe("Pokedex", function () {
-  it("front page can be opened", function () {
-    cy.visit("http://localhost:5000")
-    cy.contains("ivysaur")
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur"')
     cy.contains(
-      "Pokémon and Pokémon character names are trademarks of Nintendo."
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
 
-  it("a pokemon deck can be opened", () => {
-    cy.visit("http://localhost:5000")
-    cy.contains("ivysaur").click()
-    cy.contains("overgrow")
-    cy.contains("chlorophyll")
+  it('a pokemon deck can be opened', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
   })
 })
